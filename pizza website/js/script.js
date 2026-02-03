faf4993835260bde09c46b0818517a6ffafedc6d
nandhitha-web <nandhithavenkatesan123@gmail.com>
2026-02-03 16:53:44 +0530

    Initial commit

---
+++ pizza website/js/script.js
@@ -0,0 +1,13 @@
+function login() {
+  alert("Login successful!");
+  window.location.href = "menu.html";
+}
+
+function register() {
+  alert("Registration successful!");
+  window.location.href = "index.html";
+}
+
+function order() {
+  alert("Order placed successfully!");
+}
